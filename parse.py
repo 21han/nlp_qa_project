6734881a1085ae3097fd5e11474dc9bb56e167e9
Zihan Guo <zguocollege@gmail.com>
2017-11-15 19:12:31 -0500

    Update parse.py

--- parse.py
+++ parse.py
@@ -9,6 +9,11 @@ import logging
 import json
 from nltk.parse import stanford
 
+''' 
+Our code is based on the following source
+Citation: https://www.khalidalnajjar.com/setup-use-stanford-corenlp-server-python/
+''' 
+
 class Parse:
 
     def __init__(self, host='http://localhost', port=9000):
@@ -19,7 +24,41 @@ class Parse:
             'pipelineLanguage': 'en',
             'outputFormat': 'json'
         }
-    def main(self):
+
+    def word_tokenize(self, sentence):
+        return self.nlp.word_tokenize(sentence)
+
+    def pos(self, sentence):
+        return self.nlp.pos_tag(sentence)
+
+    def ner(self, sentence):
+        return self.nlp.ner(sentence)
+
+    def parse(self, sentence):
+        return self.nlp.parse(sentence)
+
+    def dependency_parse(self, sentence):
+        return self.nlp.dependency_parse(sentence)
+
+    def annotate(self, sentence):
+        return json.loads(self.nlp.annotate(sentence, properties=self.props))
+
+    @staticmethod
+    def tokens_to_dict(_tokens):
+        tokens = defaultdict(dict)
+        for token in _tokens:
+            tokens[int(token['index'])] = {
+                'word': token['word'],
+                'lemma': token['lemma'],
+                'pos': token['pos'],
+                'ner': token['ner']
+            }
+        return tokens
+
+    def parse(self, sentence):
+        return self.nlp.parse(sentence)
+
+    def main(self, k):
         # 1. tokenize chunk of raw string into sentence
         tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
         target_dir = sys.argv[1]
@@ -28,11 +67,14 @@ class Parse:
         sentences = (tokenizer.tokenize(raw_data))
         # 2. filter out ill-format sentences := ones contain new line symbol
         sentences = [si for si in sentences if "\n" not in si]
-        
+        # 3. get top 10 shortest sentences
+        sentences_top_k = sorted(sentences, key = len)[:k]
+        # parsed_tree_sentences = [self.parse(si) for si in sentences]
+        for i in sentences_top_k:
+            print(self.parse(i))        
 
-run = Parse()
-run.main()
 
 if __name__ == '__main__':
     run = Parse()
-    run.main()
+    # user defined top K shortest sentences in a wiki article
+    run.main(20)
